0b22a8acbaf5c0da060aef7a5838ac5a16ee1df1
Benjamin Barbe <benjamin@pandasuite.com>
2025-09-30 16:29:37 +0200

    Update ESLint configuration, remove package-lock.json, and upgrade dependencies
    - Updated ESLint configuration to extend with Prettier and set ECMAScript version to 2020.
    - Removed package-lock.json to avoid conflicts with Yarn.
    - Upgraded various dependencies in package.json, including React and ESLint packages, to their latest versions for improved performance and compatibility.

--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,14 @@ import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/stable';
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { PandaBridgeRoot } from 'pandasuite-bridge-react';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'));
+root.render(
   <PandaBridgeRoot>
     <App />
   </PandaBridgeRoot>,
-  document.getElementById('root'),
 );
