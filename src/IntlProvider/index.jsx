44d9e4f900435ecdd1c4eec8e3c54c8c8eedc7d2
Benjamin Barbe <benjamin@pandasuite.com>
2025-12-05 11:52:09 +0100

    Add localization support for multiple languages in IntlProvider
    - Introduced Spanish, Italian, Portuguese, Dutch, and German translations.
    - Updated IntlProvider to handle new languages and fallback logic.
    - Enhanced existing English and French translations with a "Forgot your password?" label.
    - Added corresponding JSON files for each new language with necessary translations.

--- src/IntlProvider/index.jsx
+++ src/IntlProvider/index.jsx
@@ -4,26 +4,51 @@ import startsWith from 'lodash/startsWith';
 
 import messagesEn from '../constants/intl/en.json';
 import messagesFr from '../constants/intl/fr.json';
+import messagesEs from '../constants/intl/es.json';
+import messagesIt from '../constants/intl/it.json';
+import messagesPt from '../constants/intl/pt.json';
+import messagesNl from '../constants/intl/nl.json';
+import messagesDe from '../constants/intl/de.json';
 
 const IntlProvider = (props) => {
   // eslint-disable-next-line react/prop-types
   const { children, language } = props;
-  let locale = language || navigator.language.substring(0, 2);
+  let locale = (language || navigator.language || 'en').toLowerCase();
 
   if (startsWith(locale, 'fr')) {
     locale = 'fr';
+  } else if (startsWith(locale, 'es')) {
+    locale = 'es';
+  } else if (startsWith(locale, 'it')) {
+    locale = 'it';
+  } else if (startsWith(locale, 'pt')) {
+    locale = 'pt';
+  } else if (startsWith(locale, 'nl')) {
+    locale = 'nl';
+  } else if (startsWith(locale, 'de')) {
+    locale = 'de';
   } else {
     locale = 'en';
   }
+
   const localizedMessages = {
     fr: messagesFr,
     en: messagesEn,
+    es: messagesEs,
+    it: messagesIt,
+    pt: messagesPt,
+    nl: messagesNl,
+    de: messagesDe,
   };
+
+  const localeMessages = localizedMessages[locale] || messagesEn;
+  const messages =
+    locale === 'en' ? messagesEn : { ...messagesEn, ...localeMessages };
 
   return (
     <NativeIntlProvider
       locale={locale}
-      messages={localizedMessages[locale]}
+      messages={messages}
       textComponent={<></>}
     >
       {children}
